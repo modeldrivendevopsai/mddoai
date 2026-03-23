5fc68f6106a0282994bf40ed8cb22111aac66b64
Akshat Dhingra <101332986+Dhingraakshat@users.noreply.github.com>
2026-03-23 09:43:17 +0200

    modular_main

---
+++ main/src/main/java/PsmToGitlabHandler.java
@@ -0,0 +1,43 @@
+package main.java;
+
+import java.io.File;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import com.mddoai.metamodel.gitlab.gitlabMM.GitlabMMPackage;
+import main.java.mddoai.generators.GeneratorExecutor;
+import main.java.mddoai.loaders.ModelLoader;
+
+public class PsmToGitlabHandler {
+
+    public static void handle(String inputModelPath, String outputFolder) {
+        ResourceSet resourceSet = new ResourceSetImpl();
+        try {
+            EObject inputModel = ModelLoader.loadModel(inputModelPath, resourceSet, EObject.class);
+
+            if (inputModel == null) {
+                System.err.println("Failed to load Gitlab model: " + inputModelPath);
+                System.exit(1);
+            }
+
+            if (inputModel.eClass().getEPackage() != GitlabMMPackage.eINSTANCE) {
+                System.err.println("Input model should be a GitLab metamodel instance.");
+                System.exit(1);
+            }
+
+            GeneratorExecutor.execute(inputModel, "gitlab", outputFolder);
+
+            File[] files = new File(outputFolder).listFiles();
+            if (files == null || files.length == 0) {
+                System.err.println("No files were generated in output folder: " + outputFolder);
+                System.exit(1);
+            }
+
+            System.out.println("GitLab YAML Code has been generated...");
+        } catch (Exception e) {
+            System.err.println("Error during transformation process: " + e.getMessage());
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+}
